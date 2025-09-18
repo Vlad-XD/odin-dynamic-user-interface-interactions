bf7c36aec9bcf3515aedc031049aa1beeb129a04
Vlad <192461579+Vlad-XD@users.noreply.github.com>
2025-09-17 19:34:19 -0700

    Implement the basic image carousel functionality

---
+++ scripts/imageCarousel.js
@@ -0,0 +1,99 @@
+/* createImageCarousel: takes several elements to produce an image carousel.
+                        Additionally, takes an optional argument, establishing
+                        a timeout to automatically advance the slide every
+                        passed seconds.
+*/
+
+function createImageCarousel(prevBtn, nextBtn, navDotsContainer, dotVisibilityClass, carouselTranslateFactorProperty, timeout=false) {
+
+  // variable declarations
+  const navCircularArray = new carouselNavArray(navDotsContainer, dotVisibilityClass, carouselTranslateFactorProperty);
+
+  prevBtn.addEventListener("click", () => {
+    navCircularArray.prev();
+  })
+
+  nextBtn.addEventListener("click", () => {
+    navCircularArray.next();
+  })
+
+
+  // add timeout functionality if argument provided
+  if (timeout !== false) {
+
+  }
+
+}
+
+// helper class to create a circular array 
+class circularArray {
+
+  #iterator = 0; // #terator will be utilized to run through array
+  #array; // holds array which will be made circular
+  #maxIndex = 0; // holds the max index size of the passed array
+
+  constructor(array) {
+    this.#array = array;
+    this.#maxIndex = array.length - 1;
+  }
+
+  // move to next index
+  next() {
+    (this.#iterator === this.#maxIndex) ? this.#iterator = 0 : this.#iterator++;
+  }
+
+  // move to previous index
+  prev() {
+    (this.#iterator === 0) ? this.#iterator = this.#maxIndex : this.#iterator--;
+  }
+
+  // return element of array where iterator is currently at
+  getCurrentElement() {
+    return this.#array[this.#iterator];
+  }
+
+  // return index where iterator is at
+  getCurrentIndex(){
+    return this.#iterator;
+  }
+
+} 
+
+// helper class that extends the circular array class to control the navigation
+// dots and current carousel image
+class carouselNavArray extends circularArray {
+
+    #dotVisibilityClass; // controls the appearance of the currently active nav dominant-baseline: 
+    #carouselTranslateFactorProperty; // css property used to control current image shown by carousel
+    #currentNavDot; // holds currently active navigation dot element
+    #root = document.documentElement; // holds root element of document
+
+  constructor(navDotsContainer, dotVisibilityClass, carouselTranslateFactorProperty) {
+    const navDotsArray = Array.from(navDotsContainer.children);
+    super(navDotsArray);
+    this.#dotVisibilityClass = dotVisibilityClass;
+    this.#carouselTranslateFactorProperty = carouselTranslateFactorProperty;
+    this.#currentNavDot = navDotsArray[0];
+  }
+
+  // move to next image
+  next() {
+    this.#currentNavDot.classList.toggle(this.#dotVisibilityClass);
+    super.next();
+    this.#currentNavDot = this.getCurrentElement();
+    this.#currentNavDot.classList.toggle(this.#dotVisibilityClass);
+    this.#root.style.setProperty(this.#carouselTranslateFactorProperty, this.getCurrentIndex());
+  }
+
+  // move to previous image
+  prev() {
+    this.#currentNavDot.classList.toggle(this.#dotVisibilityClass);
+    super.prev();
+    this.#currentNavDot = this.getCurrentElement();
+    this.#currentNavDot.classList.toggle(this.#dotVisibilityClass);
+    this.#root.style.setProperty(this.#carouselTranslateFactorProperty, this.getCurrentIndex());
+  }
+
+}
+
+export {createImageCarousel};
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,21 @@
 import { createDropdown } from "./dropdownMenu.js";
+import { createImageCarousel } from "./imageCarousel.js";
 
 //variable declarations
 const dropdownVisibilityClass = "hidden";
 const dropdownColorPropertyName = "--dropdown-menu-text-color";
+const carouselNavDotVisibilityClass = "dot-active";
+const carouselTranslateFactorProperty = "--image-carousel-translate-factor";
 
 // find DOM elements
 const root = document.documentElement;
-const dropdownSectionHeading = document.querySelector(".dropdown-menu-heading");
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdownOptionsMenu = document.querySelector(".dropdown-options");
 const dropdownOptionsList = document.querySelectorAll(".dropdown-options li");
+const carouselTrack = document.querySelector(".carousel-track");
+const carouselPrevBtn = document.querySelector(".nav-btn.left-nav-btn");
+const carouselNextBtn = document.querySelector(".nav-btn.right-nav-btn");
+const carouselNavDotsContainer = document.querySelector(".carousel-nav-dot-container");
 
 // create dropdown menu
 createDropdown(dropdownButton, dropdownOptionsMenu, dropdownOptionsList, dropdownVisibilityClass);
@@ -20,3 +26,6 @@ dropdownOptionsList.forEach((item) => {
     root.style.setProperty(dropdownColorPropertyName, item.textContent);
   })
 })
+
+// create dropdown menu
+createImageCarousel(carouselPrevBtn, carouselNextBtn, carouselNavDotsContainer, carouselNavDotVisibilityClass, carouselTranslateFactorProperty)
